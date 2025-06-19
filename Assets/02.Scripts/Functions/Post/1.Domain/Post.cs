147e66a07ce7f736a1b0ca325cff1fe423795401
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-06-19 11:17:11 +0900

    Post도메인 수정

--- Assets/02.Scripts/Functions/Post/1.Domain/Post.cs
+++ Assets/02.Scripts/Functions/Post/1.Domain/Post.cs
@@ -1,26 +1,38 @@
 ﻿using System.Collections.Generic;
 using Firebase.Firestore;
-[FirestoreData]
 public class Post
 {
-	[FirestoreProperty]
-	public string Email { get; set; }
-	[FirestoreProperty]
-	public string Nickname { get; set; }
-	[FirestoreProperty]
-	public string PostID { get; set; }
-	[FirestoreProperty]
-	public string Content { get; set; }
-	[FirestoreProperty]
-	public int Like { get; set; }
-	[FirestoreProperty]
-	public int ViewCount { get; set; }
-	[FirestoreProperty]
-	public Timestamp PostTime { get; set; }
+	public string Email { get; }
+	public string Nickname { get; }
+	public string PostID { get; }
+	public string Content { get; }
+	public int Like { get; }
+	public int ViewCount { get; }
+	public Timestamp PostTime { get; }
+	public Post() { }
+
 
 	// 댓글 리스트 
 	public List<Comment> Comments = new List<Comment>();
 	// 어카운트
-	
 
+	public Post(string email, string nickname, string postID, string content, int like = 0, int viewCount = 0)
+		: this(email, nickname, postID, content, like, viewCount, Timestamp.GetCurrentTimestamp())
+	{
+	}
+
+	public Post(string email, string nickname, string postID, string content, int like, int viewCount, Timestamp postTime)
+	{
+		Email = email;
+		Nickname = nickname;
+		PostID = postID;
+		Content = content;
+		Like = like;
+		ViewCount = viewCount;
+		PostTime = postTime;
+	}
+	public PostDTO ToDto()
+	{
+		return new PostDTO(Email, Nickname, PostID, Content, Like, ViewCount, PostTime);
+	}
 }
--- Assets/02.Scripts/Functions/Post/1.Domain/PostDTO.cs
+++ Assets/02.Scripts/Functions/Post/1.Domain/PostDTO.cs
@@ -1,7 +1,35 @@
-﻿namespace _02.Scripts.Functions.Post._1.Domain
+﻿using Firebase.Firestore;
+[FirestoreData]
+public class PostDTO
 {
-	public class PostDTO
+	[FirestoreProperty]
+	public string Email { get; private set; }
+	[FirestoreProperty]
+	public string Nickname { get; private set; }
+	[FirestoreProperty]
+	public string PostID { get; private set; }
+	[FirestoreProperty]
+	public string Content { get; private set; }
+	[FirestoreProperty]
+	public int Like { get; private set; }
+	[FirestoreProperty]
+	public int ViewCount { get; private set; }
+	[FirestoreProperty]
+	public Timestamp PostTime { get; private set; }
+	public PostDTO()
 	{
+
+	}
+	public PostDTO(string email, string nickname, string postID, string content, int like, int viewCount, Timestamp postTime)
+	{
+		Email = email;
+		Nickname = nickname;
+		PostID = postID;
+		Content = content;
+		Like = like;
+		ViewCount = viewCount;
+		PostTime = postTime;
 		
 	}
+	
 }
