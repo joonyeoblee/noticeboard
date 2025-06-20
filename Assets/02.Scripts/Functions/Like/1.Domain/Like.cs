af401eb4046e88a5105c0fa367ff4d3957f3d16b
GuardKim <kimkyoungho0726@gmail.com>
2025-06-20 14:26:04 +0900

    feat. Like Speicification 추가중

--- Assets/02.Scripts/Functions/Like/1.Domain/Like.cs
+++ Assets/02.Scripts/Functions/Like/1.Domain/Like.cs
@@ -1,4 +1,5 @@
-﻿public enum ETargetType
+﻿using System;
+public enum ETargetType
 {
 	Post = 0,
 	Comment = 1,
@@ -16,6 +17,22 @@ public class Like
 
 	public Like(string email, string nickname, string targetID, string likeID, int targetType)
 	{
+		
+		AccountEmailSpecification emailSpecification = new AccountEmailSpecification();
+		if (!emailSpecification.IsSatisfiedBy(email)) throw new Exception(emailSpecification.ErrorMessage);
+
+		AccountNicknameSpecification nicknameSpecification = new AccountNicknameSpecification();
+		if (!nicknameSpecification.IsSatisfiedBy(nickname)) throw new Exception(nicknameSpecification.ErrorMessage);
+
+		LikeTargetIdSpecification targetIdSpecification = new LikeTargetIdSpecification();
+		if (!targetIdSpecification.IsSatisfiedBy(targetID)) throw new Exception(targetIdSpecification.ErrorMessage);
+
+		LikeIdSpecification likeIdSpecification = new LikeIdSpecification();
+		if (!likeIdSpecification.IsSatisfiedBy(likeID)) throw new Exception(likeIdSpecification.ErrorMessage);
+
+		LikeTargetTypeSpecification targetTypeSpecification = new LikeTargetTypeSpecification();
+		if (!targetTypeSpecification.IsSatisfiedBy(targetType)) throw new Exception(targetTypeSpecification.ErrorMessage);
+
 		Email = email;
 		Nickname = nickname;
 		TargetID = targetID;
